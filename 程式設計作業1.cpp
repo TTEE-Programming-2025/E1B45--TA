32eacb1507d10fd7b71c57b6cb6d1b2beb689914
邱宇廷 <u11302245@o365.ttu.edu.tw>
2025-03-29 01:06:02 +0800

    fourth commit  */ 這代表第4次編譯，第一次交就寫 first commit /* 在這裡寫上你的程式修改了甚麼    */ 每次commit都要，並且最後一次commit 要寫心得 /*

---
+++ 程式設計作業1.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void second (void); 
+int main (void){
+	int password ;
+	char word;
+		printf("==============================\n") ;
+		printf("::                          ==\n") ;
+		printf("==                          ==\n") ;
+		printf("==           你好           ::\n");
+		printf("==                          ==\n") ;
+		printf("::                          ==\n") ;
+		printf("==============================\n") ;
+		printf("::                          ==\n") ;
+		printf("==                          ==\n") ;
+		printf("==        按任意按鍵        ::\n");
+		printf("==                          ==\n") ;
+		printf("::                          ==\n") ;
+		printf("==============================\n") ;
+		getche();
+		system("cls");
+
+		printf("請輸入密碼\n");
+		scanf("%d",&password);
+		
+		if (password==2025){ 
+			printf("歡迎\n");
+			system("pause");} 
+		else { 
+			printf("\a輸入錯誤！\n");
+        	system("pause");
+        	return 0;} 
+		system("cls");
+		second();
+		
+	
+		
+		
+		system("pause");
+        return 0;
+}
+void second (void){
+		char word;
+		
+		printf("==============================\n") ;
+		printf("==   'A'...'Z' : Uppercase  ==\n");
+		printf("==   'a'...'z' : Lowercase  ==\n");
+		printf("==   '0'...'9' : Digit      ==\n");
+		printf("==   Otherwise : Your name  ==\n");
+		printf("==============================\n") ;
+		
+		printf("請輸入字元");
+		fflush(stdin);
+		scanf("%c",&word);
+		
+		if (word>='a'&& word<='z'){
+			printf(" Lowercase\n");
+		}
+		else if (word>='A'&& word<='Z'){
+			printf(" Uppercase\n");
+		}
+		else if (word>='0'&& word<='9'){
+			printf(" Uppercase\n");
+		}
+		else{
+			printf(" E1B45 邱宇廷 \n");
+		}
+	
+		return  ;	
+}
