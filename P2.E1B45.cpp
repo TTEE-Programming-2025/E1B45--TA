7c244da43694a5402b7ebadf06723afac5faf631
unknown <u11302245@o365.ttu.edu.twgit config --user.name >
2025-04-23 20:39:28 +0800

    third commit 打了未完成的倒三角形

--- P2.E1B45.cpp
+++ P2.E1B45.cpp
@@ -25,7 +25,36 @@ void Main_Menu(void){
 		printf("==                             ==\n");
 		printf("=================================\n") ;
 } 
-
+int Multiplication_table (int n){
+	int i,w;
+	
+	printf("請輸入1 至 9 的整數 n :");
+	scanf("%d",&n);
+	
+	while(n<1||n>9){
+	printf("錯誤，重輸 :");
+	scanf("%d",&n);	}
+	
+	for  (i=1;i<=n;i++){
+		for  (w=1;w<=n;w++){
+			printf("%d*%d=%d ",i,w,i*w);	}
+		printf("\n");}
+	return n;}
+char Right_triangle(){
+	char word;
+	printf("請輸入a 至 n 的 字元 :");
+	scanf("%c",&word);
+	while(n<'a'||n>'n'){
+	printf("錯誤，重輸 :");
+	scanf("%c",&word);	}
+	for  (i=1;i<=n;i++){
+		for  (s=1;s<=n;s++){
+			for  (w=1;w<=n;w++){ 
+			
+			printf("%d*%d=%d ",i,w,i*w);	}
+		printf("\n");}
+}
+	
 int main (void){
 	int n;
 	password(n);
@@ -35,6 +64,7 @@ int main (void){
 	Main_Menu();
 	getchar ();
 	system("CLS");
+	Multiplication_table(n);
 
 	return 0;
 }
