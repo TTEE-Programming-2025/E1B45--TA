7d5e13b8ecac2f6022dec994bb35f839b3ea968f
unknown <u11302245@o365.ttu.edu.twgit config --user.name >
2025-04-27 15:39:12 +0800

    last commit 透過這次作業，我了解了副程式結束後主程式仍會繼續執行，也更熟悉了副程式的使用。同時我也發現自己寫的程式碼有些凌亂、太複雜了，之後會更注意程式的清晰度和簡潔性。

--- P2.E1B45.cpp
+++ P2.E1B45.cpp
@@ -2,16 +2,7 @@
 #include<stdlib.h>
 #include<conio.h>
 
-int password (int n){
-	int x=0;
-	
-	printf("請輸入密碼 :");
-	scanf("%d",&n);
-	x++;
-
-	
-	return n;
-}
+// 主選單顯示函式
 void Main_Menu(void){
 		printf("=================================\n") ;
 		printf("==                             ==\n");
@@ -21,32 +12,41 @@ void Main_Menu(void){
 		printf("==                             ==\n");
 		printf("=================================\n") ;
 } 
+// 乘法表功能
 int Multiplication_table (int n){
 	int i,w;
 	
 	printf("請輸入1 至 9 的整數 n :");
 	scanf("%d",&n);
 	
+	// 檢查輸入是否正確
 	while(n<1||n>9){
 	printf("錯誤，重輸 :");
 	scanf("%d",&n);	}
 	
+	// 印出n*n的乘法表
 	for  (i=1;i<=n;i++){
 		for  (w=1;w<=n;w++){
 			printf("%d*%d=%d ",i,w,i*w);	}
 		printf("\n");}
 	return n;}
+	
+// 畫出直角三角形
 char Right_triangle(){
 	int i=1,j=1,k=1,n,lines;
 	char word;
 	printf("請輸入a 至 n 的 字元 :");
-	fflush(stdin);
+	fflush(stdin);		// 清除輸入緩衝
 	scanf("%c",&word);
 	n=word;
+	
+	// 檢查輸入是否在 'a' ~ 'n' 範圍內
 	while(word<'a'||word>'n'){
 	printf("錯誤，重輸 :");
 	scanf("%c",&word);  	}
 	n=word;
+	
+	// 根據輸入字元，畫出直角三角形
 	for  (i=n-'a';i>=0;i--){
 		for  (j='a'+i;j>='a'+1;j--){
 			printf(" ");
@@ -57,19 +57,11 @@ char Right_triangle(){
 		}
 		printf("\n");} 
 }
-int ask(void){
-	char c;
-	printf("Continue? (y/n) :");
-	fflush(stdin);
-	c=getchar();
-	printf("%c",c);
-	if (c=='n'||c=='N'){
-		return 0;	}
-		
-}
+// 主程式
 int main (void){
 	int n,x;
 	char c;
+		//個性介面 
  		printf("==============================\n") ;
  		printf("==        		    ::\n");
  		printf("==         (  歡迎  )       ::\n");
@@ -92,6 +84,7 @@ int main (void){
  		printf("==      [ 按任意按鍵 ]      ==\n") ;
  		printf("::                          ==\n") ;
  		printf("==============================\n") ;
+ 	//判斷密碼是否正確 
 	do{
 	printf("輸入一個密碼 :");
 	scanf("%d",&n);
@@ -106,6 +99,7 @@ int main (void){
 	fflush(stdin);
 	getchar ();
 	system("CLS");
+	//判斷 a,b,c 的條件 
 	do{
 	Main_Menu();
 	fflush(stdin);
