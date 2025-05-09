21b64308bbb65000b490fa7addec96c01141ecaa
unknown <u11302245@o365.ttu.edu.twgit config --user.name >
2025-05-09 08:42:03 +0800

    first commit 打了密碼，跟主選單

---
+++ P3.E1B45.cpp
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int main (void){
+	int n,x;
+	char c;
+		//­Ó©Ê¤¶­± 
+ 		printf("==============================\n") ;
+ 		printf("==        		    ::\n");
+ 		printf("==         (  Åwªï  )       ::\n");
+ 		printf("==        		    ::\n");
+ 		printf("::  ªôªôªô   ªô   ªôªôªô    ==\n") ;
+ 		printf("==  ªô       ªô   ªô   ªô   ==\n") ;
+ 		printf("==  ªôªôªô   ªô   ªô  ªô    ==\n") ;
+ 		printf("==  ªô       ªô   ªô   ªô   ::\n");
+ 		printf("==  ªôªôªô   ªô   ªôªôªô    ==\n") ;
+ 		printf("==                          ==\n") ;
+ 		printf("::                          ==\n") ;
+ 		printf("==============================\n") ;
+ 		printf("==                          ==\n") ;
+ 		printf("::    ªô ªô      ªôªôªôªô   ==\n") ;
+ 		printf("==   ªô  ªô      ªô         ==\n") ;
+ 		printf("==  ªôªôªôªôªô    ªôªôªô    ::\n");
+ 		printf("==       ªô            ªô   ==\n") ;
+ 		printf("::       ªô      ªôªôªôªô   ==\n") ;
+ 		printf("==                          ::\n") ;
+ 		printf("==      [ «ö¥ô·N«öÁä ]      ==\n") ;
+ 		printf("::                          ==\n") ;
+ 		printf("==============================\n") ;
+ 	//§PÂ_±K½X¬O§_¥¿½T 
+	do{
+	printf("¿é¤J¤@­Ó±K½X :");
+	scanf("%d",&n);
+	x++;
+	if(n!=2025) printf("¿ù»~¡A½Ð") ;
+	if (x==3){
+		printf("¿ù»~3¦¸");
+		return 0;}
+		} while(n!=2025);
+	printf(" \n ");
+	printf("       Åwªï       ");
+	fflush(stdin);
+	getchar ();
+	system("CLS");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;	
+} 
+	
+
+
+
+
+
+
+
+
+
+
+
+
