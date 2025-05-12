2a67dfb447c62c7331de0f61d15c07afadbfd041
unknown <u11302245@o365.ttu.edu.twgit config --user.name >
2025-05-12 09:01:15 +0800

    forth commit 我增加了b的第一題

--- P3.E1B45.cpp
+++ P3.E1B45.cpp
@@ -2,36 +2,46 @@
 #include<stdlib.h>
 #include<conio.h>
 #include <time.h>
-void command_a (){
-	int i=0,j=0,k=0;
-	char momo[9][9]	;
-	srand( time(NULL) );
-		printf("\\123456789");
-
-	for (i=0;i<9;i++){ 
-		for(j=0;j<9;j++){ 
-			momo[i][j]='-';} 
-}
-	for (i=0;i<10;i++){
-	 int a= rand() % 9,b= rand() % 9;
-			momo[a][b]='*';
-			
-}
-
+void exhibit (char momo[9][9]){
+	int i,j;
+	printf("\\123456789");
 	for (i=0;i<9;i++){
-		printf("\n%d",9-i);
-		/////印出 -
+			printf("\n%d",9-i);
 		for(j=0;j<9;j++){ 
-			printf("%c",momo[i][j]);} 
-}	
-
-	return ;	
+			printf("%c",momo[i][j]);
+			}
+		}
 	}
-	
+void cancel (char momo[9][9]){
+	int i=0,j=0,k=0;
+		for (i=0;i<9;i++){ 
+			for (j=1;j<9;j++){
+				while (momo[i][j]=='@'){
+					momo[i][j]='-';
+				}
+			}	
+		}	
+	return ;	
+}
+char change (char momo[9][9]){
+	int i=0,j=0,k=0;
+		for (i=0;i<9;i++){ 
+			for (j=1;j<9;j++){
+				while (momo[i][j]=='@'){
+					momo[i][j]='*';
+				}
+			}	
+		}	
+	return momo[9][9];	
+}
+
+		
 
 int main (void){
-	int n,x;
+	int i,j,k,a,n,x;
 	char c;
+	char momo[9][9]={'-'};
+	srand( time(NULL) );
 		////////////          個性介面 
  		printf("==============================\n") ;
  		printf("==        		    ::\n");
@@ -55,12 +65,13 @@ int main (void){
  		printf("==      [ 按任意按鍵 ]      ==\n") ;
  		printf("::                          ==\n") ;
  		printf("==============================\n") ;
+
  	//判斷密碼是否正確 
 	do{
 	printf("輸入一個密碼 :");
 	scanf("%d",&n);
-	x++;
-	if(n!=2025) printf("錯誤，請") ;
+	
+	if(n!=2025){ printf("錯誤，請"); x++;} 
 	if (x==3){
 		printf("錯誤3次");
 		return 0;}
@@ -70,7 +81,22 @@ int main (void){
 	fflush(stdin);
 	getchar ();
 	system("CLS");
+	/////////預設座位表
+	printf("\\123456789");
+	for (i=0;i<9;i++){ 
+		for(j=0;j<9;j++){ 
+			momo[i][j]='-';} 
+}
+	for (i=0;i<10;i++){
+	 int a= rand() % 9,b= rand() % 9;
+	 	while(momo[a][b]=='*'){ 
+			a= rand() % 9,b= rand() % 9;} 
+			momo[a][b]='*';	
+} 
+	
 	//////////////////////        主選單 
+	do{ 
+	system("CLS");
 	printf("---------[Booking System]----------\n" ) ;
 	printf("| a. Available seats               |\n") ;
 	printf("| b. Arrange for you               |\n") ;
@@ -78,11 +104,59 @@ int main (void){
 	printf("| d. Exit                          |\n") ;
 	printf("------------------------------------\n") ;
 	
-	char choose=getchar();
+	char choose=getch();
 	if (choose=='a'||choose=='A'){
-		command_a ();
+			exhibit(momo);
+			getche();	
 	}
-	
+	else if (choose=='b'||choose=='B'){
+		printf("請問需要幾個座位(1~4): ");	
+		scanf("%d",&k);
+		if(k==1){
+			i=rand()%9,j=rand()%9;
+				while (momo[i][j]=='-'){
+					momo[i][j]='@';
+				}
+			exhibit(momo);
+			printf("Are you satisfied? ( y/n ) :");
+			fflush(stdin);
+			scanf("%c",&a);
+			if (a=='y')
+				change(momo);	
+			if (a=='n')
+				cancel(momo); 
+			}
+		if (k==2){
+			
+			while(1){ 
+				i=rand()%9,j=rand()%8;
+				while (momo[i][j]=='-'&&momo[i][j+1]=='-'){
+					momo[i][j]='@';
+					momo[i][j+1]='@';
+					break;
+				}
+			} 
+			exhibit(momo);
+			printf("Are you satisfied? ( y/n ) :");
+			fflush(stdin);
+			scanf("%c",&a);
+			if (a=='y')
+				change(momo);	
+			if (a=='n')
+				cancel(momo); 
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+				
+		}	
+	 
+}while(1); 
 	
 	
 	
