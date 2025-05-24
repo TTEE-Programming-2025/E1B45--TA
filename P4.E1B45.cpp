9ed6ab7294d27d8cbe62ee954cc9bcce5a9a1c1c
unknown <u11302245@o365.ttu.edu.twgit config --user.name >
2025-05-24 20:19:13 +0800

    forth commit 我這次做了c

--- P4.E1B45.cpp
+++ P4.E1B45.cpp
@@ -12,8 +12,16 @@ struct data{
 	int physics;
 	int english;	
 };	struct data Qiu[10];
+	char search[]={0};
+	int num=0;
+	float sum[]={0} ,average[]={0};
+void aver(){
+	for(int i=0;i<num;i++){
+		sum[i]=Qiu[i].math+Qiu[i].english+Qiu[i].physics;
+		average[i]=sum[i]/3;
+	}
+}
 void case_a (){
-	int num;
 		system("CLS");
 		do{
 		printf("輸入（5~10）的整數 :");
@@ -25,6 +33,7 @@ void case_a (){
 		system("CLS");
 		printf("輸入第%d個學生姓名  : ",i+1);
 		scanf("%s",	Qiu[i].name);
+		///// ID
 		do{
 			system("CLS");
 			printf("輸入第%d個（學生ID ) : ",i+1);
@@ -32,6 +41,7 @@ void case_a (){
 			if(Qiu[i].ID<100000||Qiu[i].ID>999999)
 				printf("錯誤");
 		}while(Qiu[i].ID<100000||Qiu[i].ID>999999);
+		///// Math
 		do{
 			system("CLS");
 			printf("輸入第%d個學生(math成績) : ",i+1);
@@ -39,13 +49,7 @@ void case_a (){
 			if(Qiu[i].math<0||Qiu[i].math>100)
 				printf("錯誤");
 		}while(Qiu[i].ID<0||Qiu[i].math>100);
-		do{
-			system("CLS");
-			printf("輸入第%d個學生(math成績) : ",i+1);
-			scanf("%d",	&Qiu[i].physics);
-			if(Qiu[i].physics<0||Qiu[i].physics>100)
-				printf("錯誤");
-		}while(Qiu[i].physics<0||Qiu[i].physics>100);
+		//// physics
 		do{
 			system("CLS");
 			printf("輸入第%d個學生(physics成績) : ",i+1);
@@ -53,6 +57,7 @@ void case_a (){
 			if(Qiu[i].physics<0||Qiu[i].physics>100)
 				printf("錯誤");
 		}while(Qiu[i].physics<0||Qiu[i].physics>100);
+		/////  english
 		do{
 			system("CLS");
 			printf("輸入第%d個學生(english成績) : ",i+1);
@@ -62,6 +67,30 @@ void case_a (){
 		}while(Qiu[i].english<0||Qiu[i].english>100);
 	} 
 } 
+void case_b (){
+	system("CLS");
+	aver();
+	for(int i=0;i<num;i++){
+		printf("第%d個 姓名:%s 學號:%d math:%d phy:%d Engl:%d avg:%.1f\n",i+1,Qiu[i].name,Qiu[i].ID,Qiu[i].math,Qiu[i].physics,Qiu[i].english,average[i]);
+	}
+	system("pause");
+}
+void case_c (){
+	int m;
+	system("CLS");
+	printf("請輸入要檢索的姓名 :");
+	scanf("%s",&search);
+	for(int i=0;i<num;i++){ 
+	if(strcmp(search,Qiu[i].name)==0) {
+		m=i;
+		printf("第%d個 姓名:%s 學號:%d math:%d phy:%d Engl:%d avg:%.1f\n",m+1,Qiu[m].name,Qiu[m].ID,Qiu[m].math,Qiu[m].physics,Qiu[m].english,average[m]);
+	}
+	else{
+		printf("資料不存在");
+	}
+}
+	system("pause");
+}
 int main (){
     struct data Qiu[10];
 	int password,k=0,c,num,i;
@@ -112,8 +141,12 @@ int main (){
 		if(c=='a'||c=='A'){
 			case_a();
 		}
-		
-	
+		if(c=='b'||c=='B'){
+			case_b();
+		}
+		if(c=='c'||c=='C'){
+			case_c();
+		}
 	}while(1);
 	return 0;
 }
